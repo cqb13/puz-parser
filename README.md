18e8acc3778e98409e80c2892c4b602f10a6b816
cqb13 <cqb13.main@gmail.com>
2025-10-16 23:42:28 -0400

    added README

---
+++ README.md
@@ -0,0 +1,5 @@
+# PUZ Reader
+
+A .PUZ file decoder and encoder that strictly follows the format specified [here](https://code.google.com/archive/p/puz/wikis/FileFormat.wiki).
+
+_Note that that extra sections are not supported and data in Reserved/Unknown sections of the file is lost when reading_
--- main.go
+++ main.go
@@ -2,25 +2,39 @@ package main
 
 import (
 	"dev/cqb13/puz-reader/puz"
+	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
-	fp, err := os.Open("crossword.puz")
+	var basePath = "./tests/test-files/"
+	entries, err := os.ReadDir(basePath)
 	if err != nil {
 		panic(err)
 	}
 
-	bytes, err := io.ReadAll(fp)
-	if err != nil {
-		panic(err)
-	}
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
 
-	puzzle, err := puz.DecodePuz(bytes)
-	if err != nil {
-		panic(err)
-	}
+		fp, err := os.Open(basePath + entry.Name())
+		if err != nil {
+			fmt.Printf("Failed to open %s: %s\n", entry.Name(), err)
+			continue
+		}
+		defer fp.Close()
 
-	puzzle.Display()
+		bytes, err := io.ReadAll(fp)
+		if err != nil {
+			fmt.Printf("Failed to read %s: %s\n", entry.Name(), err)
+			continue
+		}
+
+		_, err = puz.DecodePuz(bytes)
+		if err != nil {
+			fmt.Printf("Failed to parse %s: %s\n", entry.Name(), err)
+		}
+	}
 }
